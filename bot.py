333ed24d4c19731e7e7f46441a8411381135c82a
Danilo Alexandrino <danilo.alexandrinodm@gmail.com>
2019-12-16 17:26:32 -0300

    Adicionando botões

--- bot.py
+++ bot.py
@@ -1,15 +1,35 @@
 # encoding: utf-8
 import telebot
+from telebot import types
 
 token = raw_input('Digite o token do seu chatbot:')
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(token) 
 
-@bot.message_handler(commands=['start', 'help'])
+# Mensagem Inicial
+@bot.message_handler(commands=['start', '9'])
 def send_welcome(message):
-	bot.reply_to(message, "Olá, como posso ajudar?")
+	markup = types.ReplyKeyboardMarkup()
+	itembtn1 = types.KeyboardButton('/1')
+	itembtn2 = types.KeyboardButton('/2')
+	itembtn3 = types.KeyboardButton('/3')
+	itembtn4 = types.KeyboardButton('/4')
+	itembtn5 = types.KeyboardButton('/5')
+	itembtn6 = types.KeyboardButton('/6')
+	itembtn7 = types.KeyboardButton('/7')
+	itembtn8 = types.KeyboardButton('/8')
+	itembtn9 = types.KeyboardButton('/9')
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+	markup.row(itembtn1, itembtn2, itembtn3)
+	markup.row(itembtn4, itembtn5, itembtn6)
+	markup.row(itembtn7, itembtn8, itembtn9)
+	bot.reply_to(message, "Olá, como posso ajudar?"
+	"\n1 - Como abrir e acompanhar tickets?"
+	"\n9 - Exibir Opções",
+	reply_markup=markup)
+
+# Opções do cliente
+@bot.message_handler(commands=['1'])
+def echo_all_cupom(message):
+	bot.reply_to(message, "https://bleez.zendesk.com/hc/pt-br/articles/360033185432-Como-abrir-e-acompanhar-tickets-")
 
 bot.polling()

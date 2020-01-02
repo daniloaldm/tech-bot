81634ed2e9f81488590e773e826a847924e0dc6b
Danilo Alexandrino <danilo.alexandrinodm@gmail.com>
2020-01-02 20:22:09 -0300

    Construindo fluxo

--- bot.py
+++ bot.py
@@ -1,35 +1,73 @@
 # encoding: utf-8
 import telebot
 from telebot import types
+from datetime import datetime
+import sys
 
 token = raw_input('Digite o token do seu chatbot:')
-bot = telebot.TeleBot(token) 
+bot = telebot.TeleBot(token)
 
-# Mensagem Inicial
-@bot.message_handler(commands=['start', '9'])
+now = datetime.now()
+hora = now.hour 
+if(hora>6 and hora<12):
+	msg = "Bom dia."
+elif(hora>12 and hora<18):
+	msg = "Boa Tarde."
+else:
+	msg = "Desculpe não podemos lhe responder agora, nosso horário de atendimento é de 08:00 á 12:00 e de 13:00 á 18:00."
+
+# sys.exit(), testes
+# msg="Boa noite", testes
+
+# Atendimento Inicial
+@bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	markup = types.ReplyKeyboardMarkup()
-	itembtn1 = types.KeyboardButton('/1')
-	itembtn2 = types.KeyboardButton('/2')
-	itembtn3 = types.KeyboardButton('/3')
-	itembtn4 = types.KeyboardButton('/4')
-	itembtn5 = types.KeyboardButton('/5')
-	itembtn6 = types.KeyboardButton('/6')
-	itembtn7 = types.KeyboardButton('/7')
-	itembtn8 = types.KeyboardButton('/8')
-	itembtn9 = types.KeyboardButton('/9')
+	
+	if(msg!="Desculpe não podemos lhe responder agora, nosso horário de atendimento é de 08:00 á 12:00 e de 13:00 á 18:00."):
+		bot.reply_to(
+			message, msg+", Seja bem vindo ao atendimento da Bleez.\n\n"
+			"Você está no autoatendimento da Bleez. Este"
+			" menu inicial é automático.\n"
+			"\n Digite ou clique nos comandos para prosseguir:\n"
+			"/Planos - Para aderir aos planos\n"
+			"/Suporte - Suporte Técnico\n"
+			"/Finalizar - Finalizar atendimento"
+		)
+	else:
+		bot.reply_to(message, msg)
+		bot.reply_to(
+			message, "Agradecemos seu contato\n\n"
+			"Siga a Bleez e acompanhe nossas ações...\n\n"
+			"Instagram\n"
+			"https://www.instagram.com/bleezecommerce/\n\n"
+			"Nossos contatos:\n"
+			"85 0000-0000"
+		)
 
-	markup.row(itembtn1, itembtn2, itembtn3)
-	markup.row(itembtn4, itembtn5, itembtn6)
-	markup.row(itembtn7, itembtn8, itembtn9)
-	bot.reply_to(message, "Olá, como posso ajudar?"
-	"\n1 - Como abrir e acompanhar tickets?"
-	"\n9 - Exibir Opções",
-	reply_markup=markup)
+# Primeiro menu de opções do cliente
+@bot.message_handler(commands=['Planos'])
+def echo_all(message):
+	bot.reply_to(
+		message, "Escolha o melhor Plano pro seu negócio\n\n"
+		"O Bleez Shop tem planos preparados pra atender o seu negócio de acordo com o seu nível de maturidade no e-commerce. Se você precisa validar seu negócio (Basic), se você precisa alavancar suas vendas (Pro), ou até mesmo se você precisa de uma operação exclusiva e robusta (Exclusive).\n\n"
+		"Para mais informações acesse:\n"
+		"https://bleez.com.br/plataforma-de-ecommerce-em-fortaleza/"
 
-# Opções do cliente
-@bot.message_handler(commands=['1'])
-def echo_all_cupom(message):
-	bot.reply_to(message, "https://bleez.zendesk.com/hc/pt-br/articles/360033185432-Como-abrir-e-acompanhar-tickets-")
+	)
 
+@bot.message_handler(commands=['Finalizar'])
+def echo_all(message):
+	bot.reply_to(message, "Agradecemos seu contato\n\n"
+			"Siga a Bleez e acompanhe nossas ações...\n\n"
+			"Instagram\n"
+			"https://www.instagram.com/bleezecommerce/\n\n"
+			"Nossos contatos:\n"
+			"85 0000-0000")
+
+
+@bot.message_handler(commands=['Suporte'])
+def echo_all(message):
+	bot.reply_to(message, "Suporte print")
+
+	
 bot.polling()
